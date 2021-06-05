347b0dbc3cdd30b9443437a7523682cf1790d43e
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-06-05 23:56:25 +0300

    Template kullanma ve resim galerisi

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category, Product
+from product.models import Category, Product, Images
+
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,7 +17,13 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'price', 'amount', 'status']
     list_filter = ['status', 'category', 'price']
+    inlines = [ProductImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image']
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImagesAdmin)
--- product/models.py
+++ product/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -20,13 +20,14 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
 class Product(models.Model):
-    STATUS= (
+    STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) #category ile ilişki kurma
-    title = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # category ile ilişki kurma
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -38,4 +39,14 @@ class Product(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-       return self.title
+        return self.title
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
