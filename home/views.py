d80997f421f49b2335dcbc5c1c727fb449c49d3f
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-06-15 02:24:34 +0300

    iletişim formu yapıldı

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -23,6 +25,20 @@ def referanslar(request):
 
 
 def iletisim(request):
+    if request.method == 'POST' :
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir. Teşekkür ederiz.")
+            return HttpResponseRedirect('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
