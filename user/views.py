d99bdba6ec07fe49e502c5d58510e631a7139771
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-06-29 15:22:09 +0300

    Uye profilinde bilgileri güncelleme

--- user/views.py
+++ user/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
 
 # Create your views here.
 from home.models import UserProfile
 from product.models import Category
+from user.forms import UserUpdateForm, ProfileUpdateForm
 
 
 def index(request):
@@ -16,3 +18,24 @@ def index(request):
                }
     return render(request, 'user_profile.html', context)
 
+
+@login_required(login_url='/login')
+def user_update(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user) # request.user is user  data
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return HttpResponseRedirect('/user')
+    else:
+        category = Category.objects.all()
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.userprofile) #"userprofile" model -> OneToOneField relatinon with user
+        context = {
+            'category': category,
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+        return render(request, 'user_update.html', context)
