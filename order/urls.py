42cda75958f671fe5fa9f546e2d8f3512d89ad5e
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-06-30 19:54:57 +0300

    Sipariş yapma ve tamamlama- ürünü stoktan düşme

--- order/urls.py
+++ order/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('addtocart/<int:id>', views.addtocart, name='addtocart'),
     path('deletefromcart/<int:id>', views.deletefromcart, name='deletefromcart'),
+    path('orderproduct/', views.orderproduct, name='orderproduct'),
 ]
--- order/views.py
+++ order/views.py
@@ -2,8 +2,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
 
-from order.models import ShopCart, ShopCartForm
+from home.models import UserProfile
+from order.models import ShopCart, ShopCartForm, OrderForm, Order, OrderProduct
 from product.models import Product, Category
 
 
@@ -35,7 +37,8 @@ def addtocart(request, id):
                 data.product_id = id
                 data.quantity = form.cleaned_data['quantity']
                 data.save()
-            request.session['cart_items'] = ShopCart.objects.filter(user_id=current_user.id).count() #shopcartta ürün saydırma
+            request.session['cart_items'] = ShopCart.objects.filter(
+                user_id=current_user.id).count()  # shopcartta ürün saydırma
             messages.success(request, "Ürün başarı ile sepete eklendi. ")
             return HttpResponseRedirect(url)
 
@@ -50,7 +53,8 @@ def addtocart(request, id):
             data.product_id = id
             data.quantity = 1
             data.save()
-        request.session['cart_items'] = ShopCart.objects.filter(user_id=current_user.id).count()  # shopcartta ürün saydırma
+        request.session['cart_items'] = ShopCart.objects.filter(
+            user_id=current_user.id).count()  # shopcartta ürün saydırma
         messages.success(request, "Ürün başarı ile sepete eklendi. ")
         return HttpResponseRedirect(url)
 
@@ -63,6 +67,7 @@ def shopcart(request):
     category = Category.objects.all()
     current_user = request.user
     schopcart = ShopCart.objects.filter(user_id=current_user.id)
+    request.session['cart_items'] = ShopCart.objects.filter(user_id=current_user.id).count()
     total = 0
     for rs in schopcart:
         total += rs.product.price * rs.quantity
@@ -74,7 +79,69 @@ def shopcart(request):
     return render(request, 'shopcart_products.html', context)
 
 
+@login_required(login_url='/login')
 def deletefromcart(request, id):
     ShopCart.objects.filter(id=id).delete()
+    current_user = request.user
+    request.session['cart_items'] = ShopCart.objects.filter(user_id=current_user.id).count()
     messages.success(request, "Ürün sepetten silindi.")
     return HttpResponseRedirect("/shopcart")
+
+
+@login_required(login_url='/login')
+def orderproduct(request):
+    category = Category.objects.all()
+    current_user = request.user
+    schopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total = 0
+    for rs in schopcart:
+        total += rs.product.price * rs.quantity
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            data = Order()
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.address = form.cleaned_data['address']
+            data.city = form.cleaned_data['city']
+            data.phone = form.cleaned_data['phone']
+            data.user_id = current_user.id
+            data.total = total
+            data.ip = request.META.get('REMOTE_ADDR')
+            ordercode = get_random_string(5).upper()
+            data.code = ordercode
+            data.save()
+
+            schopcart = ShopCart.objects.filter(user_id=current_user.id)
+            for rs in schopcart:
+                detail = OrderProduct()
+                detail.order_id = data.id
+                detail.product_id = rs.product.id
+                detail.user_id = current_user.id
+                detail.quantity = rs.quantity
+                detail.price = rs.product.price
+                detail.amount = rs.amount
+                detail.save()
+
+                product = Product.objects.get(id=rs.product_id)
+                product.amount -= rs.quantity
+                product.save()
+
+            ShopCart.objects.filter(user_id=current_user.id).delete()
+            request.session['cart_items'] = 0
+            messages.success(request, "Siparişiniz başarı ile tamamlandı, teşekkürler")
+            return render(request, 'order_completed.html', {'ordercode': ordercode, 'category': category})
+        else:
+            messages.warning(request, form.errors)
+            return HttpResponseRedirect("/order/orderproduct")
+
+    form = OrderForm()
+    profile = UserProfile.objects.get(user_id=current_user.id)
+    context = {'schopcart': schopcart,
+               'category': category,
+               'total': total,
+               'form': form,
+               'profile': profile,
+               }
+    return render(request, 'order_form.html', context)
