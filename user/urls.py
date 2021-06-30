89ea0eb7e625bddd07329c0a98befea637ed0047
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-06-30 20:39:57 +0300

    Üye Şifre Güncelleme

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # ex: /home/
     path('', views.index, name='index'),
     path('update/', views.user_update, name='user_update'),
+    path('password/', views.change_password, name='change_password'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from home.models import UserProfile
@@ -22,7 +24,7 @@ def index(request):
 @login_required(login_url='/login')
 def user_update(request):
     if request.method == 'POST':
-        user_form = UserUpdateForm(request.POST, instance=request.user) # request.user is user  data
+        user_form = UserUpdateForm(request.POST, instance=request.user)  # request.user is user  data
         profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -32,10 +34,28 @@ def user_update(request):
     else:
         category = Category.objects.all()
         user_form = UserUpdateForm(instance=request.user)
-        profile_form = ProfileUpdateForm(instance=request.user.userprofile) #"userprofile" model -> OneToOneField relatinon with user
+        profile_form = ProfileUpdateForm(
+            instance=request.user.userprofile)  # "userprofile" model -> OneToOneField relation with user
         context = {
             'category': category,
             'user_form': user_form,
             'profile_form': profile_form
         }
         return render(request, 'user_update.html', context)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Şifreniz başarılı bir şekilde güncellendi.')
+            return HttpResponseRedirect('/user')
+        else:
+            messages.error(request, 'Hata! <br>' + str(form.errors))
+            return HttpResponseRedirect('/user/password')
+    else:
+        category = Category.objects.all()
+        form = PasswordChangeForm(request.user)
+        return render(request, 'change_password.html', {'form': form, 'category': category})
